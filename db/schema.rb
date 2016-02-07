6a183223c5dc17774c7e484828c5ded4548bf1e8
Devin Clark <devin.clark27@gmail.com>
2016-02-07 11:09:47 -0600

    Adding subpages for site

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207163543) do
+ActiveRecord::Schema.define(version: 20160207170751) do
+
+  create_table "concerts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "news", force: :cascade do |t|
     t.string   "title"
