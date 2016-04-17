bdd119141510a72ddc79d56c6b5c67b1d95bb5fe
Devin Clark <devin.clark27@gmail.com>
2016-04-17 17:22:01 -0500

    Adding fav icon / Securing image uploads

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   # config/environments/production.rb
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV['BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
