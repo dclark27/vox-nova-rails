7378ff45668a1eda1a9ecc73b59a0e82cb724690
Devin Clark <devin.clark27@gmail.com>
2016-04-17 17:25:56 -0500

    Securing images once more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   #production.rb
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV['BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
